83ee0153fa21bce07ab2c187a5585d734c0163fd
Lorance, Rylan Case <lorance1@llnl.gov>
2023-11-06 14:19:48 -0800

    added theme

---
+++ src/Theme.js
@@ -0,0 +1,19 @@
+
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#F9B572',
+    },
+    secondary: {
+      main: '#748E63',
+    },
+    background: {
+      paper: '#FAF8ED',
+    },
+  },
+})
+
+export default theme;
