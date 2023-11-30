c63f4849d4f7eb313dd7abbe704e4e1dacff4b10
Rylan Lorance <rylancl@gmail.com>
2023-11-30 13:02:06 -0800

    Add API Checking

--- src/components/Recipe/RecipeBook/RecipeBook.js
+++ src/components/Recipe/RecipeBook/RecipeBook.js
@@ -1,20 +1,38 @@
-import React, { useState } from 'react';
-import { Box, Divider, Typography } from '@mui/material';
-import data from '../../../recipe_harcoded/recipe.json'
-import RecipeCard from './RecipeCard/RecipeCard';
-import RecipeSearchBox from './RecipeSearchBox/RecipeSearchBox';
-import RecipeBookMain from './RecipeBookMain/RecipeBookMain';
+import React, { useState } from "react";
+import { Box, Divider, Typography } from "@mui/material";
+import data from "../../../recipe_harcoded/recipe.json";
+import RecipeCard from "./RecipeCard/RecipeCard";
+import RecipeSearchBox from "./RecipeSearchBox/RecipeSearchBox";
+import RecipeBookMain from "./RecipeBookMain/RecipeBookMain";
+import useFetch from "../../../hooks/useFetch";
 
 function RecipeBook(props) {
-    const [searchResults, setSearchResults] = useState(data)
-    
-    return (
+  const [searchResults, setSearchResults] = useState(data);
+
+  const { isLoading, serverError, apiData } = useFetch(
+    "GET",
+    "http://localhost:8585/recipes",
+    ""
+  );
+
+  console.log("apiData", apiData);
+  console.log("serverError", serverError);
+
+  // setSearchResults()
+
+  return (
+    <div>
+      {!isLoading && serverError ? (
+        <span>Error in fetching data ...</span>
+      ) : (
         <div>
-            <RecipeSearchBox></RecipeSearchBox>
-            <hr></hr>
-            <RecipeBookMain recipes={searchResults}></RecipeBookMain>
+          <RecipeSearchBox></RecipeSearchBox>
+          <hr></hr>
+          <RecipeBookMain recipes={searchResults}></RecipeBookMain>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default RecipeBook;
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function useFetch(method, url, body) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [apiData, setApiData] = useState();
+  const [serverError, setServerError] = useState(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchData = async () => {
+      try {
+        const resp = await axios({
+          method: method,
+          url: url,
+          data: body,
+        });
+
+        const data = await resp?.data;
+        console.log('innter data', resp)
+
+        setApiData(data);
+        setIsLoading(false);
+      } catch (error) {
+
+        setServerError(error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url, method, body]);
+
+  return { isLoading, apiData, serverError };
+}
