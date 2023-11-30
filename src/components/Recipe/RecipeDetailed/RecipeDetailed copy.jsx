ee9502eeac793f912021ddfe8917bf3432ecda89
Rylan Lorance <rylancl@gmail.com>
2023-11-30 12:17:39 -0800

    Commit

---
+++ src/components/Recipe/RecipeDetailed/RecipeDetailed copy.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import recipeHardcoded from "../../../recipe_harcoded/recipe.json";
+import { Box, Container, Paper, Tab, Tabs, Typography } from "@mui/material";
+import RecipeDetailedIngredients from "./LeftBox/Tabs/RecipeDetailedIngredients/RecipeDetailedIngredients";
+import RecipeDetailedInstructions from "./LeftBox/Tabs/RecipeDetailedInstructions";
+import RecipeDetailedNotes from "./LeftBox/Tabs/RecipeDetailedNotes";
+
+import './RecipeDetailed.css';
+import RecipeDetailedRightBox from "./RightBox/RecipeDetailedRightBox";
+
+function RecipeDetailed(props) {
+  const recipe = recipeHardcoded[0];
+
+  const [tabValue, setTabValue] = useState(0);
+
+  function handleTabChange(event, newValue) {
+    setTabValue(newValue);
+  }
+
+  return (
+    
+    <Box mt={2} className="recipe-detailed-box">
+
+      
+      <Box className="recipe-detailed-left-box">
+        <Typography variant="h4">{recipe.title}</Typography>
+        <Box>
+          <Tabs value={tabValue} onChange={handleTabChange}>
+            <Tab
+              label="Ingredients"
+              id="simple-tab-0"
+              aria-controls="simple-tabpanel-0"
+            ></Tab>
+            <Tab
+              label="Instructions"
+              id="simple-tab-1"
+              aria-controls="simple-tabpanel-1"
+            ></Tab>
+            <Tab
+              label="Notes"
+              id="simple-tab-2"
+              aria-controls="simple-tabpanel-2"
+            ></Tab>
+          </Tabs>
+        </Box>
+        <RecipeDetailedIngredients value={tabValue} index={0} recipe={recipe}/>
+        <RecipeDetailedInstructions value={tabValue} index={1} recipe={recipe}/>
+        <RecipeDetailedNotes value={tabValue} index={2} />
+      </Box>
+      <Box className="recipe-detailed-right-box">
+        <RecipeDetailedRightBox></RecipeDetailedRightBox>
+      </Box>
+    </Box>
+  );
+}
+
+export default RecipeDetailed;
--- src/components/Recipe/RecipeDetailed/RecipeDetailed.jsx
+++ src/components/Recipe/RecipeDetailed/RecipeDetailed.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import recipeHardcoded from "../../../recipe_harcoded/recipe.json";
-import { Box, Container, Paper, Tab, Tabs, Typography } from "@mui/material";
+import { Box, Container, Grid, Paper, Tab, Tabs, Typography } from "@mui/material";
 import RecipeDetailedIngredients from "./LeftBox/Tabs/RecipeDetailedIngredients/RecipeDetailedIngredients";
 import RecipeDetailedInstructions from "./LeftBox/Tabs/RecipeDetailedInstructions";
 import RecipeDetailedNotes from "./LeftBox/Tabs/RecipeDetailedNotes";
@@ -19,34 +19,39 @@ function RecipeDetailed(props) {
 
   return (
     <Box mt={2} className="recipe-detailed-box">
-      <Box className="recipe-detailed-left-box">
-        <Typography variant="h4">{recipe.title}</Typography>
-        <Box>
-          <Tabs value={tabValue} onChange={handleTabChange}>
-            <Tab
-              label="Ingredients"
-              id="simple-tab-0"
-              aria-controls="simple-tabpanel-0"
-            ></Tab>
-            <Tab
-              label="Instructions"
-              id="simple-tab-1"
-              aria-controls="simple-tabpanel-1"
-            ></Tab>
-            <Tab
-              label="Notes"
-              id="simple-tab-2"
-              aria-controls="simple-tabpanel-2"
-            ></Tab>
-          </Tabs>
-        </Box>
-        <RecipeDetailedIngredients value={tabValue} index={0} recipe={recipe}/>
-        <RecipeDetailedInstructions value={tabValue} index={1} recipe={recipe}/>
-        <RecipeDetailedNotes value={tabValue} index={2} />
-      </Box>
-      <Box className="recipe-detailed-right-box">
-        <RecipeDetailedRightBox></RecipeDetailedRightBox>
-      </Box>
+      <Grid container>
+        <Grid item xs={7}>
+          <Box className="recipe-detailed-left-box">
+            <Typography variant="h4">{recipe.title}</Typography>
+            <Box>
+              <Tabs value={tabValue} onChange={handleTabChange}>
+                <Tab
+                  label="Ingredients"
+                  id="simple-tab-0"
+                  aria-controls="simple-tabpanel-0"
+                ></Tab>
+                <Tab
+                  label="Instructions"
+                  id="simple-tab-1"
+                  aria-controls="simple-tabpanel-1"
+                ></Tab>
+                <Tab
+                  label="Notes"
+                  id="simple-tab-2"
+                  aria-controls="simple-tabpanel-2"
+                ></Tab>
+              </Tabs>
+            </Box>
+            <RecipeDetailedIngredients value={tabValue} index={0} recipe={recipe} />
+            <RecipeDetailedInstructions value={tabValue} index={1} recipe={recipe} />
+            <RecipeDetailedNotes value={tabValue} index={2} />
+          </Box>
+        </Grid>
+        <Grid item xs={5}>
+          <RecipeDetailedRightBox></RecipeDetailedRightBox>
+
+        </Grid>
+      </Grid>
     </Box>
   );
 }
